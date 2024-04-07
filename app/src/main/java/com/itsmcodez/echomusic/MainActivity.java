09d160ddc40f90718a6cfe871dc48d27de487912
Solomon Mensah <iam.itsmcodez@gmail.com>
2024-04-07 13:46:31 +0000

    Implement BottomNavigation item selections in MainActivity.java

--- app/src/main/java/com/itsmcodez/echomusic/MainActivity.java
+++ app/src/main/java/com/itsmcodez/echomusic/MainActivity.java
@@ -4,7 +4,14 @@ package com.itsmcodez.echomusic;
 import android.view.Menu;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import com.itsmcodez.echomusic.databinding.ActivityMainBinding;
+import com.itsmcodez.echomusic.fragments.AlbumsFragment;
+import com.itsmcodez.echomusic.fragments.ArtistsFragment;
+import com.itsmcodez.echomusic.fragments.PlaylistsFragment;
+import com.itsmcodez.echomusic.fragments.SongsFragment;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -22,31 +29,45 @@ public class MainActivity extends AppCompatActivity {
         // ActionBar
         setSupportActionBar(binding.toolbar);
         
+        // Fragments
+        SongsFragment songsFragment = new SongsFragment();
+        AlbumsFragment albumsFragment = new AlbumsFragment();
+        ArtistsFragment artistsFragment = new ArtistsFragment();
+        PlaylistsFragment playlistsFragment = new PlaylistsFragment();
+        replaceFragment(songsFragment);
+        
         // BottomNavigation
         binding.bottomNav.setOnItemSelectedListener( item -> {
                 
                 if(item.getItemId() == R.id.songs_menu_item) {
-                    
+                    replaceFragment(songsFragment);
                     return true;
                 }
                 
                 if(item.getItemId() == R.id.albums_menu_item) {
-                    
+                    replaceFragment(albumsFragment);
                     return true;
                 }
                 
                 if(item.getItemId() == R.id.artists_menu_item) {
-                    
+                    replaceFragment(artistsFragment);
                     return true;
                 }
                 
                 if(item.getItemId() == R.id.playlists_menu_item) {
-                    
+                    replaceFragment(playlistsFragment);
                     return true;
                 }
                 
                 return false;
         });
+    }
+    
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentHolder, fragment);
+        fragmentTransaction.commit();
     }
     
     @Override
