fb7652fa3343c2adff0fac15c7a7a83ce7fee36c
Solomon Mensah <iam.itsmcodez@gmail.com>
2024-04-09 19:02:56 +0000

    Request for runtime storage permission

--- app/src/main/java/com/itsmcodez/echomusic/MainActivity.java
+++ app/src/main/java/com/itsmcodez/echomusic/MainActivity.java
@@ -1,11 +1,15 @@
 
 package com.itsmcodez.echomusic;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -27,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
         // set content view to binding's root
         setContentView(binding.getRoot());
+        
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        	ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            return;
+        }
         
         // ActionBar
         setSupportActionBar(binding.toolbar);
@@ -95,5 +104,14 @@ public class MainActivity extends AppCompatActivity {
         
         return super.onOptionsItemSelected(item);
     }
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        	recreate();
+        }
+    }
+    
     
 }
