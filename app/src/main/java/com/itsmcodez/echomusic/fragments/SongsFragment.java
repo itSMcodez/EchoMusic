b36fdb2c1cb52be384ff4b73d3c2b3698fb68512
Solomon Mensah <iam.itsmcodez@gmail.com>
2024-04-09 19:03:42 +0000

    Implement livedata

--- app/src/main/java/com/itsmcodez/echomusic/fragments/SongsFragment.java
+++ app/src/main/java/com/itsmcodez/echomusic/fragments/SongsFragment.java
@@ -4,15 +4,41 @@ import android.view.ViewGroup;
 import android.os.Bundle;
 import android.view.View;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.itsmcodez.echomusic.adapters.SongsAdapter;
 import com.itsmcodez.echomusic.databinding.FragmentSongsBinding;
+import com.itsmcodez.echomusic.models.SongsModel;
+import com.itsmcodez.echomusic.viewmodels.SongsViewModel;
+import java.util.ArrayList;
 
 public class SongsFragment extends Fragment {
     private FragmentSongsBinding binding;
+    private SongsAdapter songsAdapter;
+    private SongsViewModel songsViewModel;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        songsViewModel = new ViewModelProvider(this).get(SongsViewModel.class);
+    }
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Bind to views
         binding = FragmentSongsBinding.inflate(inflater, container, false);
+        
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(container.getContext(), RecyclerView.VERTICAL, false));
+        
+        songsViewModel.getAllSongs().observe(getViewLifecycleOwner(), new Observer<ArrayList<SongsModel>>() {
+                @Override
+                public void onChanged(ArrayList<SongsModel> allSongs) {
+                	songsAdapter = new SongsAdapter(container.getContext(), getLayoutInflater(), allSongs);
+                    binding.recyclerView.setAdapter(songsAdapter);
+                }
+        });
         
         return binding.getRoot();
     }
