0f8f7c62cd452c501205850109f76897fe2fd178
Solomon Mensah <iam.itsmcodez@gmail.com>
2024-04-10 20:26:10 +0000

    Add AlbumsAdapter.java

---
+++ app/src/main/java/com/itsmcodez/echomusic/adapters/AlbumsAdapter.java
@@ -0,0 +1,67 @@
+package com.itsmcodez.echomusic.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.itsmcodez.echomusic.R;
+import com.itsmcodez.echomusic.databinding.LayoutAlbumItemBinding;
+import com.itsmcodez.echomusic.models.AlbumsModel;
+import de.hdodenhof.circleimageview.CircleImageView;
+import java.util.ArrayList;
+
+public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.AlbumsViewHolder> {
+    private LayoutAlbumItemBinding binding;
+    private Context context;
+    private LayoutInflater inflater;
+    private ArrayList<AlbumsModel> albums;
+
+    public AlbumsAdapter(Context context, LayoutInflater inflater, ArrayList<AlbumsModel> albums) {
+        this.context = context;
+        this.inflater = inflater;
+        this.albums = albums;
+    }
+
+    public static class AlbumsViewHolder extends RecyclerView.ViewHolder {
+        private RelativeLayout itemView;
+        private CircleImageView albumArtwork;
+        private TextView title;
+
+        public AlbumsViewHolder(LayoutAlbumItemBinding binding) {
+            super(binding.getRoot());
+            this.itemView = binding.itemView;
+            this.albumArtwork = binding.albumArtwork;
+            this.title = binding.title;
+        }
+    }
+
+    @Override
+    public AlbumsAdapter.AlbumsViewHolder onCreateViewHolder(ViewGroup parent, int position) {
+        // Bind to views
+        binding = LayoutAlbumItemBinding.inflate(inflater, parent, false);
+        return new AlbumsViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(AlbumsAdapter.AlbumsViewHolder viewHolder, int position) {
+        AlbumsModel album = albums.get(position);
+        
+        viewHolder.title.setText(album.getAlbum());
+        viewHolder.albumArtwork.setImageURI(album.getAlbumArtwork());
+        if(viewHolder.albumArtwork.getDrawable() == null) {
+        	viewHolder.albumArtwork.setImageDrawable(context.getDrawable(R.drawable.ic_album_outline));
+        }
+        
+        viewHolder.itemView.setOnClickListener(view -> {
+                
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return albums.size();
+    }
+}
