7fd0fe2cbaec72d3c7515ae4c54628064cae5073
Solomon Mensah <iam.itsmcodez@gmail.com>
2024-05-21 08:29:20 +0000

    [PlaylistSongsActivity.java] release MediaController future

--- app/src/main/java/com/itsmcodez/echomusic/PlaylistSongsActivity.java
+++ app/src/main/java/com/itsmcodez/echomusic/PlaylistSongsActivity.java
@@ -141,9 +141,12 @@ public class PlaylistSongsActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         this.binding = null;
-        if(!mediaController.isPlaying()) {
-            stopService(new Intent(this, MusicService.class));
-        }
+    }
+    
+    @Override
+    public void onStop() {
+        super.onStop();
+        MediaController.releaseFuture(controllerFuture);
     }
     
     @Override
