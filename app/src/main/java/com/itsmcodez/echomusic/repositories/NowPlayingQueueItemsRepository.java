82a3e525b026347dfc4f2330854ecaccac7d4761
Solomon Mensah <iam.itsmcodez@gmail.com>
2024-06-11 13:22:33 +0000

    Add NowPlayingQueueItemsRepository.java

---
+++ app/src/main/java/com/itsmcodez/echomusic/repositories/NowPlayingQueueItemsRepository.java
@@ -0,0 +1,36 @@
+package com.itsmcodez.echomusic.repositories;
+
+import android.app.Application;
+import androidx.lifecycle.MutableLiveData;
+import com.itsmcodez.echomusic.models.NowPlayingQueueItemsModel;
+import java.util.ArrayList;
+
+public class NowPlayingQueueItemsRepository {
+    private static Application application;
+    private static NowPlayingQueueItemsRepository instance;
+    private ArrayList<NowPlayingQueueItemsModel> songs;
+    private MutableLiveData<ArrayList<NowPlayingQueueItemsModel>> allSongs;
+
+    private NowPlayingQueueItemsRepository() {}
+
+    public static synchronized NowPlayingQueueItemsRepository getInstance(Application application) {
+        NowPlayingQueueItemsRepository.application = application;
+        if (instance == null) {
+            instance = new NowPlayingQueueItemsRepository();
+        }
+        return NowPlayingQueueItemsRepository.instance;
+    }
+
+    public ArrayList<NowPlayingQueueItemsModel> getSongs() {
+        return this.songs;
+    }
+
+    public MutableLiveData<ArrayList<NowPlayingQueueItemsModel>> getAllSongs() {
+        return this.allSongs;
+    }
+
+    public void setSongs(ArrayList<NowPlayingQueueItemsModel> songs) {
+        this.songs = songs;
+        allSongs.setValue(songs);
+    }
+}
