38becceabd03132798f2da9df412b20ab5ad5c55
Solomon Mensah <iam.itsmcodez@gmail.com>
2024-06-21 12:16:13 +0000

    [CrashHandler] Update classes

--- app/src/main/java/com/itsmcodez/echomusic/CrashHandlerActivity.java
+++ app/src/main/java/com/itsmcodez/echomusic/CrashHandlerActivity.java
@@ -1,21 +1,26 @@
 package com.itsmcodez.echomusic;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.net.Uri;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import com.itsmcodez.echomusic.databinding.ActivityCrashHandlerBinding;
 import java.io.InputStream;
 
-public class CrashHandlerActivity extends Activity {
-    String[] exceptionType = {
+public class CrashHandlerActivity extends AppCompatActivity {
+    private ActivityCrashHandlerBinding binding;
+    private String[] exceptionType = {
 			"StringIndexOutOfBoundsException",
 			"IndexOutOfBoundsException",
 			"ArithmeticException",
 			"NumberFormatException",
 			"ActivityNotFoundException"
 	};
-	String[] errMessage= {
+	private String[] errMessage= {
 			"Invalid string operation\n",
 			"Invalid list operation\n",
 			"Invalid arithmetical operation\n",
@@ -25,6 +30,10 @@ public class CrashHandlerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Bind to views
+        binding = ActivityCrashHandlerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
 		Intent intent = getIntent();
 		String errMsg = "";
 		String madeErrMsg = "";
@@ -44,15 +53,39 @@ public class CrashHandlerActivity extends Activity {
 				if(madeErrMsg.isEmpty()) madeErrMsg = errMsg;
 			}catch(Exception e){}
 		}
-        AlertDialog.Builder bld = new AlertDialog.Builder(this);
-		bld.setTitle("An error occured");
-		bld.setMessage( madeErrMsg );
-		bld.setNeutralButton("End Application", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				finish();
-			}
-		});
-		bld.create().show();
+        
+        binding.crashMsg.setText(madeErrMsg);
+        
+        binding.close.setOnClickListener(view -> {
+                finish();
+        });
+        
+        final String crashLog = madeErrMsg;
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
+        clipboard.addPrimaryClipChangedListener(new ClipboardManager.OnPrimaryClipChangedListener() {
+            @Override
+            public void onPrimaryClipChanged() {
+                Toast.makeText(getApplicationContext(), "Crash Log copied!", Toast.LENGTH_LONG).show();
+            }
+        });
+        binding.report.setOnClickListener(view -> {
+                ClipData clip = ClipData.newPlainText("CRASH_LOG", crashLog);
+                clipboard.setPrimaryClip(clip);
+                try {
+                	
+                    Intent launchGithub = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.github.com:itSMcodez/EchoMusic.git"));
+                    launchGithub.addCategory(Intent.CATEGORY_BROWSABLE);
+                    launchGithub.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(launchGithub);
+                    
+                } catch(ActivityNotFoundException err) {
+                	
+                }
+        });
+        
+        binding.copy.setOnClickListener(view -> {
+                ClipData clip = ClipData.newPlainText("CRASH_LOG", crashLog);
+                clipboard.setPrimaryClip(clip);
+        });
     }
 }
