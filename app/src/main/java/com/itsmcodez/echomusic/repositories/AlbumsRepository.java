349e0f47e4684cf657ba3acdf46a5a375230024e
Solomon Mensah <iam.itsmcodez@gmail.com>
2024-06-22 13:08:23 +0000

    [AlbumsRepository.java] query albums on threadpools

--- app/src/main/java/com/itsmcodez/echomusic/repositories/AlbumsRepository.java
+++ app/src/main/java/com/itsmcodez/echomusic/repositories/AlbumsRepository.java
@@ -10,47 +10,65 @@ import com.itsmcodez.echomusic.common.SortOrder;
 import com.itsmcodez.echomusic.models.AlbumsModel;
 import com.itsmcodez.echomusic.models.SongsModel;
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class AlbumsRepository {
     private static Application application;
     private static AlbumsRepository instance;
     private ArrayList<AlbumsModel> albums;
     private MutableLiveData<ArrayList<AlbumsModel>> allAlbums;
+    private ExecutorService executor = Executors.newFixedThreadPool(4);
     
     private AlbumsRepository(){
         albums = new ArrayList<>();
         allAlbums = new MutableLiveData<>();
-        queryAlbums(albums, SortOrder.ASCENDING);
+        Future<ArrayList<AlbumsModel>> albumsFuture = executor.submit(queryAlbums(SortOrder.ASCENDING));
+        try {
+            albums = albumsFuture.get();
+    		allAlbums.setValue(removeDuplicates(albums));
+    	} catch(Exception err) {
+    		err.printStackTrace();
+    	}
     }
     
-    private void queryAlbums(ArrayList<AlbumsModel> albums, SortOrder sortOrder) {
+    private Callable<ArrayList<AlbumsModel>> queryAlbums(SortOrder sortOrder) {
     	
         if(albums.size() > 0) {
         	albums.clear();
         }
         
-        Uri albumsUri = MediaProperties.MEDIA_STORE_EXTERNAL_URI;
-        String[] projection = { MediaProperties.COLUMN_ALBUM, MediaProperties.COLUMN_ALBUM_ARTIST, MediaProperties.COLUMN_ALBUM_ID};
-        String selection = MediaProperties.IS_MUSIC;
+        ArrayList<AlbumsModel> albums = new ArrayList<>();
+        Callable<ArrayList<AlbumsModel>> albumsCallable = () -> {
         
-        String songsSortOrder = sortOrder == SortOrder.ASCENDING ? MediaProperties.SORT_ALBUM_TITLE_ASC : MediaProperties.SORT_ALBUM_TITLE_DESC;
-        
-        Cursor cursor = application.getContentResolver().query(albumsUri, projection, selection, null, songsSortOrder);
-        if(cursor.moveToFirst()) {
-        	while(cursor.moveToNext()) {
-                String album = cursor.getString(0);
-                String albumArtist = cursor.getString(1);
-                String albumId = cursor.getString(2);
+            Uri albumsUri = MediaProperties.MEDIA_STORE_EXTERNAL_URI;
+            String[] projection = { MediaProperties.COLUMN_ALBUM, MediaProperties.COLUMN_ALBUM_ARTIST, MediaProperties.COLUMN_ALBUM_ID};
+            String selection = MediaProperties.IS_MUSIC;
             
-                long _albumId = Long.parseLong(albumId);
-                Uri artworkPath = Uri.parse(MediaProperties.ALBUM_ART_URI);
-                Uri albumArtwork = ContentUris.withAppendedId(artworkPath, _albumId);
+            String songsSortOrder = sortOrder == SortOrder.ASCENDING ? MediaProperties.SORT_ALBUM_TITLE_ASC : MediaProperties.SORT_ALBUM_TITLE_DESC;
             
-                albums.add(new AlbumsModel(album, albumArtist, albumId, albumArtwork));
+            Cursor cursor = application.getContentResolver().query(albumsUri, projection, selection, null, songsSortOrder);
+            if(cursor.moveToFirst()) {
+                while(cursor.moveToNext()) {
+                    String album = cursor.getString(0);
+                    String albumArtist = cursor.getString(1);
+                    String albumId = cursor.getString(2);
+                
+                    long _albumId = Long.parseLong(albumId);
+                    Uri artworkPath = Uri.parse(MediaProperties.ALBUM_ART_URI);
+                    Uri albumArtwork = ContentUris.withAppendedId(artworkPath, _albumId);
+                
+                    albums.add(new AlbumsModel(album, albumArtist, albumId, albumArtwork));
+                }
+                cursor.close();
             }
-            cursor.close();
-        }
-        allAlbums.setValue(removeDuplicates(albums));
+            
+            return albums;
+         };
+        
+        return albumsCallable;
     }
     
     public static synchronized AlbumsRepository getInstance(Application application) {
